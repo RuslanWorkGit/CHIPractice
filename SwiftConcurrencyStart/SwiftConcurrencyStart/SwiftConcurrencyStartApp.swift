55a3bd4d8d2ebcf4366d414d7648f12fe527d7fc
RuslanWorkGit <90621533+RuslanWorkGit@users.noreply.github.com>
2025-11-10 16:00:10 +0200

    Start concurency proj

---
+++ SwiftConcurrencyStart/SwiftConcurrencyStart/SwiftConcurrencyStartApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConcurrencyStartApp.swift
+//  SwiftConcurrencyStart
+//
+//  Created by user on 10.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConcurrencyStartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
