ea21aa264c92122c76d1222d105776ef0557e996
RuslanWorkGit <90621533+RuslanWorkGit@users.noreply.github.com>
2025-11-25 22:54:30 +0200

    Create empty project

---
+++ PetProject/PetProject/PetProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetProjectApp.swift
+//  PetProject
+//
+//  Created by user on 25.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct PetProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
